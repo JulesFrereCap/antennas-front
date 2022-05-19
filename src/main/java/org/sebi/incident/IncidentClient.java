49c1320aefbc31d667c1be3a3326edf8f9e88061
jfrere <jules.frere@capgemini.com>
2022-05-19 14:42:28 +0200

    fix

--- src/main/java/org/sebi/incident/IncidentClient.java
+++ src/main/java/org/sebi/incident/IncidentClient.java
@@ -1,5 +1,4 @@
 package org.sebi.incident;
-
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.GET;
@@ -8,10 +7,10 @@ import javax.ws.rs.QueryParam;
 import java.util.List;
 import java.util.Set;
 
-@RegisterRestClient(baseUri = "http://antennas-incident-hackathon-j2-3.rhd-ams03-may16-412318946-4c50a18a6ae19b704aa10d04d75751f8-0000.ams03.containers.appdomain.cloud")
+@Path("/rest/incidents")
+@RegisterRestClient
 public interface IncidentClient {
 
     @GET
-    @Path("/rest/incidents")
     Set<Incident> getIncidents(@QueryParam("api_key") String key);
 }
